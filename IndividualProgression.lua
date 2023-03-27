3fd778d4b49bdfdb03e1ea23431fd57ef17473b3
Dinkledork <118951051+Day36512@users.noreply.github.com>
2023-03-27 00:05:31 -0600

    Add files via upload

--- IndividualProgression.lua
+++ IndividualProgression.lua
@@ -4,9 +4,6 @@
 -- INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES (50000, 0, 0, 0, 0, 0, 28195, 0,-- remove the "--"
 -- .npc add 50000 somewhere in game
 
-RegisterPlayerEvent(3, OnPlayerLogin)
-
-
 local npcId = 50000
 local mainMenu = "Set Individual Progression"
 local options = {
@@ -72,4 +69,3 @@ end
 
 
 RegisterCreatureEvent(npcId, 5, OnCreatureSpawn)
-
