ee077186a825c97b4937aae4cf35d0809f8c590c
Dinkledork <118951051+Day36512@users.noreply.github.com>
2023-04-25 16:15:47 -0600

    Add files via upload
    Fixes messing with other settings. Adds a whisper to tell player what level they are on currently.

--- IndividualProgression.lua
+++ IndividualProgression.lua
@@ -1,35 +1,53 @@
 -- by Dinkledork
--- Run the Following SQL Query:
--- DELETE FROM `creature_template` WHERE `entry`=50000;
--- INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES (50000, 0, 0, 0, 0, 0, 28195, 0,-- remove the "--"
 -- .npc add 50000 somewhere in game
 
 local npcId = 50000
-local mainMenu = "Set Individual Progression"
+local mainMenu = "|TInterface\\icons\\inv_helmet_74:45:45:-40|t|cff00008bSet Individual Progression |r"
 local options = {
-  "|TInterface\\icons\\achievement_boss_ragnaros:40|t |cff8b0000Tier 1 - Molten Core (Level 60)|r",
-  "|TInterface\\icons\\achievement_boss_onyxia:40|t |cff8b0000Tier 2 - Onyxia (Level 60)|r",
-  "|TInterface\\icons\\achievement_boss_nefarion:40|t |cff8b0000Tier 3 - Blackwing Lair (Level 60)|r",
-  "|TInterface\\icons\\achievement_zone_silithus_01:40|t |cff8b0000Tier 4 - Pre-AQ (Level 60)|r",
-  "|TInterface\\icons\\achievement_boss_cthun:40|t |cff8b0000Tier 5 - Anh'qiraj (Level 60)|r",
-  "|TInterface\\icons\\achievement_boss_kelthuzad_01:40|t |cff8b0000Tier 6 - Naxxramas (Level 60)|r",
-  "|TInterface\\icons\\achievement_boss_princemalchezaar_02:40|t |cff006400Tier 7 - Karazhan, Gruul's Lair, Magtheridon's Lair (Level 70)|r",
-  "|TInterface\\icons\\achievement_character_bloodelf_male:40|t |cff006400Tier 8 - Serpentshrine Cavern, Tempest Keep (Level 70)|r",
-  "|TInterface\\icons\\achievement_boss_illidan:40|t |cff006400Tier 9 - Hyjal Summit and Black Temple (Level 70)|r",
-  "|TInterface\\icons\\achievement_boss_zuljin:40|t |cff006400Tier 10 - Zul'Aman (Level 70)|r",
-  "|TInterface\\icons\\achievement_boss_kiljaedan:40|t |cff006400Tier 11 - Sunwell Plateau (Level 70)|r",
-  "|TInterface\\icons\\achievement_boss_kelthuzad_01:40|t |cff00008bTier 12 - Naxxramas WotLK, Eye of Eternity, Obsidian Sanctum (Level 80)|r",
-  "|TInterface\\icons\\achievement_boss_algalon_01:40|t |cff00008bTier 13 - Ulduar (Level 80)|r",
-  "|TInterface\\icons\\achievement_reputation_argentcrusader:40|t |cff00008bTier 14 - Trial of the Crusader|r",
-  "|TInterface\\icons\\achievement_boss_lichking:40|t |cff00008bTier 15 - Icecrown Citadel (Level 80)|r",
-  "|TInterface\\icons\\spell_shadow_twilight:40|t |cff00008bTier 16 - Ruby Sanctum (Level 80)"
+  "|TInterface\\icons\\achievement_boss_ragnaros:45:45:-40|t|cff8b0000Tier 1 - Molten Core (Level 60)|r",
+  "|TInterface\\icons\\achievement_boss_onyxia:45:45:-40|t|cff8b0000Tier 2 - Onyxia (Level 60)|r",
+  "|TInterface\\icons\\achievement_boss_nefarion:45:45:-40|t|cff8b0000Tier 3 - Blackwing Lair (Level 60)|r",
+  "|TInterface\\icons\\achievement_zone_silithus_01:45:45:-40|t|cff8b0000Tier 4 - Pre-AQ (Level 60)|r",
+  "|TInterface\\icons\\achievement_boss_cthun:45:45:-40|t|cff8b0000Tier 5 - Anh'qiraj (Level 60)|r",
+  "|TInterface\\icons\\achievement_boss_kelthuzad_01:45:45:-40|t|cff8b0000Tier 6 - Naxxramas (Level 60)|r",
+  "|TInterface\\icons\\achievement_boss_princemalchezaar_02:45:45:-40|t|cff006400Tier 7 - Karazhan, Gruul's Lair, Magtheridon's Lair (Level 70)|r",
+  "|TInterface\\icons\\achievement_character_bloodelf_male:45:45:-40|t|cff006400Tier 8 - Serpentshrine Cavern, Tempest Keep (Level 70)|r",
+  "|TInterface\\icons\\achievement_boss_illidan:45:45:-40|t|cff006400Tier 9 - Hyjal Summit and Black Temple (Level 70)|r",
+  "|TInterface\\icons\\achievement_boss_zuljin:45:45:-40|t|cff006400Tier 10 - Zul'Aman (Level 70)|r",
+  "|TInterface\\icons\\achievement_boss_kiljaedan:45:45:-40|t|cff006400Tier 11 - Sunwell Plateau (Level 70)|r",
+  "|TInterface\\icons\\achievement_boss_kelthuzad_01:45:45:-40|t|cff00008bTier 12 - Naxxramas WotLK, Eye of Eternity, Obsidian Sanctum (Level 80)|r",
+  "|TInterface\\icons\\achievement_boss_algalon_01:45:45:-40|t|cff00008bTier 13 - Ulduar (Level 80)|r",
+  "|TInterface\\icons\\achievement_reputation_argentcrusader:45:45:-40|t|cff00008bTier 14 - Trial of the Crusader|r",
+  "|TInterface\\icons\\achievement_boss_lichking:45:45:-40|t|cff00008bTier 15 - Icecrown Citadel (Level 80)|r",
+  "|TInterface\\icons\\spell_shadow_twilight:45:45:-40|t|cff00008bTier 16 - Ruby Sanctum (Level 80)"
 }
+
+function getTextWithoutIcon(option)
+  local textStart = option:find("|r") + 2
+  return option:sub(textStart)
+end
 
 function OnGossipHello(event, player, object)
   player:GossipMenuAddItem(0, mainMenu, 0, 1)
   player:GossipSendMenu(1, object)
   object:SetEquipmentSlots(32262, 33755, 0)
+  object:SendUnitSay("Speaking with me will allow you to artificially set what stage of the game you'd like to be in, thereby bypassing any normal progression.", 0)
+
+  local guid = player:GetGUIDLow()
+
+  local query = CharDBQuery("SELECT data FROM character_settings WHERE guid = " .. guid .. " AND source = 'mod-individual-progression'")
+
+  if query then
+    local playerProgressionTier = query:GetUInt32(0)
+    local playerProgressionInfo = options[playerProgressionTier + 1]
+
+    object:SendUnitWhisper("Your current progression level is: " .. playerProgressionInfo, 0, player)
+  else
+    object:SendUnitWhisper("You have not set any individual progression. Contact a GM for help.", 0, player)
+  end
 end
+
+
 
 local PlayerTierKey = 1000 
 
@@ -38,6 +56,10 @@ function OnGossipSelect(event, player, object, sender, intid, code)
     for i, option in ipairs(options) do
       player:GossipMenuAddItem(0, option, 0, i + 1)
     end
+    player:GossipMenuAddItem(0, "|TInterface\\icons\\achievement_guildperk_massresurrection:45:45:-40|t Back", 0, 100)
+    player:GossipSendMenu(1, object)
+  elseif intid == 100 then
+    player:GossipMenuAddItem(0, mainMenu, 0, 1)
     player:GossipSendMenu(1, object)
   else
     local tier = intid - 2
@@ -48,7 +70,6 @@ function OnGossipSelect(event, player, object, sender, intid, code)
     end
   end
 end
-
 RegisterCreatureGossipEvent(npcId, 1, OnGossipHello)
 RegisterCreatureGossipEvent(npcId, 2, OnGossipSelect)
 
@@ -56,7 +77,7 @@ function OnPlayerLogout(event, player)
   local tier = player:GetUInt32Value(PlayerTierKey)
   if tier >= 0 then
     local guid = player:GetGUIDLow()
-    CharDBExecute("UPDATE character_settings SET data = " .. tier .. " WHERE guid = " .. guid)
+    CharDBExecute("UPDATE character_settings SET data = " .. tier .. " WHERE guid = " .. guid .. " AND source = 'mod-individual-progression'")
     player:SetUInt32Value(PlayerTierKey, 0) -- Reset the value to 0
   end
 end
@@ -67,5 +88,12 @@ function OnCreatureSpawn(event, creature)
   creature:SetEquipmentSlots(32262, 33755, 0)
 end
 
+function OnPlayerLogin(event, player)
+  local creature = player:GetNearestCreature(100, npcId)
+  if creature then
+    creature:SetEquipmentSlots(32262, 33755, 0)
+  end
+end
 
+RegisterPlayerEvent(3, OnPlayerLogin)
 RegisterCreatureEvent(npcId, 5, OnCreatureSpawn)
